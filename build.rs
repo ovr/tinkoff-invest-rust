1617349710163c5db0663d44a952b7c44dba4841
Dmitry Patsura <talk@dmtry.me>
2022-02-15 16:40:38 +0300

    chore: Automatically build constracts with renaming

--- build.rs
+++ build.rs
@@ -1,21 +1,23 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // tonic_build::configure()
-    //     .build_client(true)
-    //     .build_server(false)
-    //     .out_dir("src")
-    //     // .client_mod_attribute("package", "tcs")
-    //     .compile(
-    //         &[
-    //             "contracts/instruments.proto",
-    //             "contracts/marketdata.proto",
-    //             "contracts/operations.proto",
-    //             "contracts/orders.proto",
-    //             "contracts/sandbox.proto",
-    //             "contracts/stoporders.proto",
-    //             "contracts/users.proto",
-    //         ],
-    //         &["contracts/"],
-    //     )?;
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(false)
+        .out_dir("src")
+        .format(true)
+        .compile(
+            &[
+                "contracts/instruments.proto",
+                "contracts/marketdata.proto",
+                "contracts/operations.proto",
+                "contracts/orders.proto",
+                "contracts/sandbox.proto",
+                "contracts/stoporders.proto",
+                "contracts/users.proto",
+            ],
+            &["contracts/"],
+        )?;
+
+    std::fs::rename("./src/tinkoff.public.invest.api.contract.v1.rs", "./src/tcs.rs")?;
 
     Ok(())
 }
